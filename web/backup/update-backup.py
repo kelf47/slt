8ef2531e00befee946db347c07200f0cf6ecf530
Kelf <you@example.com>
2018-04-19 12:57:01 +0200

    Backup dropbox last adjustments

--- web/backup/update-backup.py
+++ web/backup/update-backup.py
@@ -1,9 +1,11 @@
+#!/usr/local/bin/python
+
 import dropbox
-from dropbox.files import WriteMode
 import os
 
+CHUNK_SIZE = 4 * 1024 * 1024
 auth_token = "buLoD3InxtAAAAAAAAAAFLqXVfilxYOZKT86oHz9nCEsVOA9a667Si6kzLFyKGIR"
-client = dropbox.DropboxClient(auth_token)
+client = dropbox.Dropbox(auth_token)
 
 # Get all old backups in folder
 file_response = client.files_list_folder("")
@@ -14,11 +16,32 @@ if file_response:
 # Upload the new backup file
 for i in os.listdir("/tmp"):
     if os.path.isfile(os.path.join("/tmp", i)) and 'slt-backup' in i:
-        with open("/tmp/{}".format(i), 'rb') as f:
-            client.files_upload(f.read(), "/{}".format(i), mode=WriteMode(
-                'overwrite'))
-        print('uploaded')
-        break
+        f = open("/tmp/{}".format(i), 'rb')
+        file_size = os.path.getsize("/tmp/{}".format(i))
+        print("file_size: {}  max_size: {}".format(file_size, CHUNK_SIZE))
+        if file_size <= CHUNK_SIZE:
+            res = client.files_upload(f, "/{}".format(i))
+            print(res)
+        else:
+            upload_session_start_result = client.files_upload_session_start(
+                f.read(CHUNK_SIZE))
+            cursor = dropbox.files.UploadSessionCursor(
+                session_id=upload_session_start_result.session_id,
+                offset=f.tell())
+            commit = dropbox.files.CommitInfo(path="/{}".format(i))
+
+            while f.tell() < file_size:
+                if ((file_size - f.tell()) <= CHUNK_SIZE):
+                    res = client.files_upload_session_finish(
+                        f.read(CHUNK_SIZE), cursor, commit)
+                    print(res)
+                else:
+                    client.files_upload_session_append(f.read(CHUNK_SIZE),
+                                                       cursor.session_id,
+                                                       cursor.offset)
+                    cursor.offset = f.tell()
+        f.close()
+        os.remove("/tmp/{}".format(i))
 
 # Delete old backups
 for backup in old_backups:
