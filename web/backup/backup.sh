1d6c08c335c40cbbc1ce7d97cb95d68bf87f6eba
Kelf <you@example.com>
2018-06-22 13:18:49 +0200

    backup uopdate

--- web/backup/backup.sh
+++ web/backup/backup.sh
@@ -1,4 +1,4 @@
 _now=$(date +"%m_%d_%Y")
 rm /tmp/slt-backup*
-PGPASSWORD="postgres" pg_dump -h postgres -p 5432 -U postgres postgres
+PGPASSWORD="postgres" pg_dump -h postgres -p 5432 -U postgres postgres > /tmp/slt-backup-$_now.sql
 /usr/local/bin/python /usr/src/app/backup/update-backup.py
